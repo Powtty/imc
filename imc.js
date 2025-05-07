765806d558444defaa9902668a1f8a3ee482ad3b
Gabriel "Powtty <152025467+Powtty@users.noreply.github.com>
2025-05-06 22:01:39 -0300

    imcv6.js
    espero que esteja certo
    Peso e altura estavam como string, então adicionei parseFloat
    
    Corrigi o "<" de magreza, pois na v5 eu mudei para ">"
    erros de digitação foram corrigidos(eu acho)
    
    A mensagem para o usuário foi melhorada, mas não tenho certeza se terá suporte à UTF-8
    
    Isso é tudo

--- imc.js
+++ imc.js
@@ -7,18 +7,18 @@ function imc(){
         dataNasc:formDados.get("nasc"),
         telefone:formDados.get("tel"),
         sexo:document.querySelector('input[name=sexo]:checked').value,
-        peso:formDados.get("peso"),
-        altura:formDados.get("altura"),
+        peso:parseFloat(formDados.get("peso")),
+        altura: parseFloat(formDados.get("altura")),
         classificacao:"",
         calcularIMC(){
             if (this.altura > 3) this.altura=this.altura/100
             const imc = this.peso/this.altura**2;
-            if (imc > 18.5 ) this.classificacao = "magreza";
-            if (imc >= 18.5 &&  imc < 25 ) this.classificacao = "saudável"
-            if (imc >= 25 &&  imc < 30 ) this.classificacao = "sobrepeso"
-            if (imc >= 30 &&  imc < 35 ) this.classificacao = "obesidade"
-            if (imc >= 35 &&  imc < 40 ) this.classificacao = "obesidade Severa"
-            if (imc >= 40 ) this.classificacao = "obesidade móbita"
+            if (imc < 18.5 ) this.classificacao = "magreza";
+            if (imc >= 18.5 &&  imc < 25 ) this.classificacao = "Saudável";
+            if (imc >= 25 &&  imc < 30 ) this.classificacao = "Sobrepeso";
+            if (imc >= 30 &&  imc < 35 ) this.classificacao = "Obesidade";
+            if (imc >= 35 &&  imc < 40 ) this.classificacao = "Obesidade Severa";
+            if (imc >= 40 ) this.classificacao = "Obesidade mórbida";
             return imc.toFixed(2);
         
             
@@ -29,5 +29,6 @@ function imc(){
     console.log(paciente)
     console.log(paciente.calcularIMC())
     let resposta=document.getElementById('resposta');
-    resposta.innerHTML="Seu IMC"+paciente.calcularIMC()+". " + "Sua classe: " + paciente.classificacao;
+    resposta.innerHTML = "Seu IMC é " + paciente.calcularIMC() + ". Sua classificação: " + paciente.classificacao + ".";
+
 }
